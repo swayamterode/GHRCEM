9e767a811db83c509ee952b81c02dc87337742d9
Swayam Terode <81813786+swayamterode@users.noreply.github.com>
2021-06-12 15:39:27 +0530

    2.Display_name_and_roll_no_of_student.py

---
+++ Code/Programming-for-Problem-Solving-UITP102/Assignments/Assignment Number 1/2.Display_name_and_roll_no_of_student.py.py
@@ -0,0 +1,6 @@
+def personal_details():
+    name, rollNumber = "Swayam Terode", "C70"
+    College = "G H Raisoni College of Engineering & Management, Pune"
+    Branch = "Information Technology"
+    print("Name: {}\nRoll Number: {}\nCollege: {}\nBranch:{}".format(name, rollNumber, College, Branch))
+personal_details() 
